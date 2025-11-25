7a6b602226ac0d26a794c4cba7be1c9394318908
jfeofantov <jfeofantovs@gmail.com>
2025-11-25 14:06:23 +0000

    new25

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 const baseUrl = 'https://primecoatlondon.com';
 
 export default function robots(): MetadataRoute.Robots {
